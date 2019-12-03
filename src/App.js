c58a9106044a5287cff72e16352308d0ba7667ee
novalyphe <novalyphe@gmail.com>
2019-12-03 09:56:27 +0200

    Add Shop page with photos

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
      <Switch>
        <Route exact path='/' component = {HomePage} />
-       <Route  path='/' component = {ShopPage} />
+       <Route  path='/shop' component = {ShopPage} />
      </Switch>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-
 import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import CollectionPreview from '../../components/collection-prewiew/collection-preview.component';
 
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            collections:  SHOP_DATA 
+            collections:  SHOP_DATA  
         }
     }
 
     render( ){
         const {collections} = this.state;
-
-        return (
-        <div className='shop-page'>
-           {
-               collections.map( ({id, ...otherCollectionProps}) => (
+        return ( <div className='shop-page'>
+            {
+                collections.map(({ id , ...otherCollectionProps}) => (
                     <CollectionPreview key={id} {...otherCollectionProps} />
-               ))
+           )) 
            }
-        </div>
-        );
+        </div>)
     }
 }
 
